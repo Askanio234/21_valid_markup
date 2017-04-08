a4a964d80cff6e6c8dfdaf14d18dc6e0ef987e9d
Artem Chernyaev <askanio234@gmail.com>
2017-04-08 15:48:45 +0300

    Fix map-js logic and display issues

--- js/map.js
+++ js/map.js
@@ -1,14 +1,15 @@
 ymaps.ready(init);
-function init(){
-  var map = new ymaps.Map("map", {
-          center: [55.740684, 37.618174],
-          zoom: 15
-      });
-  map.behaviors.disable('scrollZoom');
-  placemark = new ymaps.Placemark([55.740684, 37.618174], {
-    hintContent: 'Твой Дом',
-    balloonContentHeader: 'Твой Дом',
-    balloonContentBody: 'Москва, Старомонетный переулок, 10'
-  });
-  map.geoObjects.add(placemark);
-}
+  function init(){
+    var map = new ymaps.Map("map", {
+            center: [55.740684, 37.618174],
+            zoom: 15
+        });
+    map.behaviors.disable('scrollZoom');
+    var placemark;
+    placemark = new ymaps.Placemark([55.740684, 37.618174], {
+      hintContent: 'Твой Дом',
+      balloonContentHeader: 'Твой Дом',
+      balloonContentBody: 'Москва, Старомонетный переулок, 10'
+    });
+    map.geoObjects.add(placemark);
+  }
